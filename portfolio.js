6619744f388280c00fefdac6c940a62b6e1a8733
pallavi0902-create <pallavichowdary0902@gmail.com>
2025-09-24 11:29:21 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,5 @@
+// Simple JavaScript function to download resume
+function downloadResume() {
+  alert("Downloading Resume...");
+  window.open("resume.pdf", "_blank"); // replace with actual resume link
+}
